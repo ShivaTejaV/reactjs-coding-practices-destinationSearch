b71ba5b3e6889a73e2531612960650993ca99cec
root <vddshivateja@gmail.com>
2022-12-08 23:15:13 +0000

    3rd

--- src/components/DestinationSearch/index.js
+++ src/components/DestinationSearch/index.js
@@ -4,19 +4,44 @@ import './index.css'
 import DestinationItem from '../DestinationItem/index'
 
 class DestinationSearch extends Component {
+  state = {searchInput: ''}
+
+  onInputChange = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
   render() {
+    const {destinationsList} = this.props
+    const {searchInput} = this.state
+    const newDestinationList = destinationsList.filter(destination =>
+      destination.name.includes(searchInput),
+    )
+    console.log(newDestinationList)
     return (
       <div className="bg">
         <h1 className="heading">Destination Search</h1>
         <div className="searchElementContainer">
-          <input type="search" placeholder="search" className="searchBar" />
+          <input
+            type="search"
+            placeholder="search"
+            className="searchBar"
+            onChange={this.onInputChange}
+          />
           <img
             src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
             alt="searchIcon"
             className="searchIcon"
           />
         </div>
-        <ul className="destinationsItemContainer"></ul>
+        <ul className="destinationsItemContainer">
+          {newDestinationList.map(eachItem => (
+            <DestinationItem
+              key={eachItem.id}
+              name={eachItem.name}
+              imgUrl={eachItem.imgUrl}
+            />
+          ))}
+        </ul>
       </div>
     )
   }
